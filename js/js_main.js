5ec6b5cc6f96e586eff6ffa30566e6850b441509
Kenny Li <k3li@ucsd.edu>
2014-09-16 00:37:54 -0700

    Fixed button not working in chrome, safari

--- js/js_main.js
+++ js/js_main.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
 	/** Section redirect buttons **/
 	setTimeout(function() {
 		$("#about_button").click(function() {
-			$('html').animate({
+			$('html,body').animate({
 				scrollTop : $("#section_2").offset().top
 			}, 700);
 		});
 		$("#education_button").click(function() {
-			$('html').animate({
+			$('html,body').animate({
 				scrollTop : $("#section_3").offset().top
 			}, 700);
 		});
 		$("#coding_button").click(function() {
-			$('html').animate({
+			$('html,body').animate({
 				scrollTop : $("#section_4").offset().top
 			}, 700);
 		});
 		$("#contact_button").click(function() {
-			$('html').animate({
+			$('html,body').animate({
 				scrollTop : $("#section_5").offset().top
 			}, 700);
 		});
