9d25dc82a9e197fd254664251666167c768dc975
Kenny Li <k3li@ucsd.edu>
2014-09-16 00:25:09 -0700

    About section (2) ok

--- js/js_main.js
+++ js/js_main.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
+	/** Section redirect buttons **/
 	setTimeout(function() {
 		$("#about_button").click(function() {
+			$('html').animate({
+				scrollTop : $("#section_2").offset().top
+			}, 700);
+		});
+		$("#education_button").click(function() {
+			$('html').animate({
+				scrollTop : $("#section_3").offset().top
+			}, 700);
+		});
+		$("#coding_button").click(function() {
 			$('html').animate({
 				scrollTop : $("#section_4").offset().top
 			}, 700);
 		});
-	}, 5000);
+		$("#contact_button").click(function() {
+			$('html').animate({
+				scrollTop : $("#section_5").offset().top
+			}, 700);
+		});
+	}, 500);
 });
